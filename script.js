16660ed42d46808ccb5ec7443e0704fee764fad1
jayakumarsharp <jayakumar407@gmail.com>
2018-09-24 19:44:03 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function callonclickevent() {
+    console.log('called method');
+    var a = 10;
+    var b = 20;
+    var c = 'chennai';
+    console.log('intialized variable');
+
+    var d = a + b;
+    console.log('after adding');
+    console.log(d);
+
+    alert('you have clicked')
+}
+
+function callonclickevent1() {
+    alert('you have clicked 1')
+}
+
+function callonclickevent2() {
+    alert('you have clicked 2')
+}
+
+function callonclickevent3() {
+    alert('you have clicked 3')
+}
