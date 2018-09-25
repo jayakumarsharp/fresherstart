7639286cddfc2b781905b4c8b45cc39b1d82714c
jayakumarsharp <jayakumar407@gmail.com>
2018-09-25 20:23:01 +0400

    commit
    learning basis

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+function pageload(){
+document.getElementById("name").value = "Johnny Bravo";
+}
+pageload();
 function callonclickevent() {
+    var name = document.getElementById('name').value;
+    alert(name);
+
     console.log('called method');
     var a = 10;
     var b = 20;
@@ -9,7 +16,7 @@ function callonclickevent() {
     console.log('after adding');
     console.log(d);
 
-    alert('you have clicked')
+    //alert('you have clicked')
 }
 
 function callonclickevent1() {
